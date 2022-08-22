049ed3551ee6a7ba7027ca3e06949e18e598cbbf
NASEEM A P <naseemalassampattil@gmail.com>
2022-08-22 11:17:11 +0530

    added webcam channel selection, added RTSP option

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ sample_img = cv2.imread('sample.jpg')
 FRAME_WINDOW = st.image(sample_img, channels='BGR')
 st.sidebar.title('Settings')
 
-options = st.sidebar.radio('Options', ('Webcam', 'Image', 'Video'), index=1)
+options = st.sidebar.radio('Options', ('Webcam', 'Image', 'Video', 'RTSP'), index=1)
 confidence = st.sidebar.slider(
     'Detection Confidence', min_value=0.0, max_value=1.0, value=0.25)
 
@@ -76,39 +76,60 @@ if options == 'Video':
                         plot_one_box(bbox, img, label=class_labels[id], color=[
                                      0, 0, 255], line_thickness=2)
                     FRAME_WINDOW.image(img, channels='BGR')
-                else:
-                    FRAME_WINDOW = st.image([])
-                    print('[INFO] Failied to Loadd Video')
-                    break
-    # else:
-    #     video_file = open('test.mp4', 'rb')
-    #     video_bytes = video_file.read()
-    #     FRAME_WINDOW.video(video_bytes)
+
 
 # Web-cam
 if options == 'Webcam':
+    cam_options = st.sidebar.selectbox('Webcam Channel',
+        ('Select Channel', '0', '1', '2', '3'))
     model = custom(path_or_model='yolov7.pt')
-    cap = cv2.VideoCapture(0)
-    while True:
-        success, img = cap.read()
-        # if pred:
-        bbox_list = []
-        results = model(img)
-        # Bounding Box
-        box = results.pandas().xyxy[0]
-        class_list = box['class'].to_list()
-        f = open('class.txt', 'r').read()
-        class_labels = f.split("\n")
-        for i in box.index:
-            xmin, ymin, xmax, ymax, conf = int(box['xmin'][i]), int(box['ymin'][i]), int(box['xmax'][i]), \
-                int(box['ymax'][i]), box['confidence'][i]
-            if conf > confidence:
-                bbox_list.append([xmin, ymin, xmax, ymax])
-        if len(bbox_list) != 0:
-            for bbox, id in zip(bbox_list, class_list):
-                plot_one_box(bbox, img, label=class_labels[id], color=[
-                             0, 0, 255], line_thickness=2)
-            FRAME_WINDOW.image(img, channels='BGR')
-        else:
-            FRAME_WINDOW = st.image([])
-            print('[INFO] Not Detetcted anything')
+    if len(cam_options)!=0:
+        if not cam_options=='Select Channel':
+            cap = cv2.VideoCapture(int(cam_options))
+            while True:
+                success, img = cap.read()
+                bbox_list = []
+                results = model(img)
+                # Bounding Box
+                box = results.pandas().xyxy[0]
+                class_list = box['class'].to_list()
+                f = open('class.txt', 'r').read()
+                class_labels = f.split("\n")
+                for i in box.index:
+                    xmin, ymin, xmax, ymax, conf = int(box['xmin'][i]), int(box['ymin'][i]), int(box['xmax'][i]), \
+                        int(box['ymax'][i]), box['confidence'][i]
+                    if conf > confidence:
+                        bbox_list.append([xmin, ymin, xmax, ymax])
+                if len(bbox_list) != 0:
+                    for bbox, id in zip(bbox_list, class_list):
+                        plot_one_box(bbox, img, label=class_labels[id], color=[
+                                    0, 0, 255], line_thickness=2)
+                    FRAME_WINDOW.image(img, channels='BGR')
+
+
+if options=='RTSP':
+    rtsp_options = st.sidebar.selectbox('RTSP Channel',
+        ('Select Channel', '0', '1', '2', '3'))
+    model = custom(path_or_model='yolov7.pt')
+    if not rtsp_options=='Select Channel':
+        cap = cv2.VideoCapture(f'rtsp://admin:eternaler4444@192.168.0.185:554/cam/realmonitor?channel={rtsp_options}&subtype=0')
+        
+        while True:
+            success, img = cap.read()
+            bbox_list = []
+            results = model(img)
+            # Bounding Box
+            box = results.pandas().xyxy[0]
+            class_list = box['class'].to_list()
+            f = open('class.txt', 'r').read()
+            class_labels = f.split("\n")
+            for i in box.index:
+                xmin, ymin, xmax, ymax, conf = int(box['xmin'][i]), int(box['ymin'][i]), int(box['xmax'][i]), \
+                    int(box['ymax'][i]), box['confidence'][i]
+                if conf > confidence:
+                    bbox_list.append([xmin, ymin, xmax, ymax])
+            if len(bbox_list) != 0:
+                for bbox, id in zip(bbox_list, class_list):
+                    plot_one_box(bbox, img, label=class_labels[id], color=[
+                                0, 0, 255], line_thickness=2)
+                FRAME_WINDOW.image(img, channels='BGR')
