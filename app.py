a0fa6de8ad2b8696f1afdbee3836f891cd4fee44
AliMostafaRadwan <omar12ali12radwan@gmail.com>
2022-11-25 00:14:06 +0200

    added a screen option

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import json
 import psutil
 import subprocess
 import pandas as pd
-
+import mss
 
 def get_gpu_memory():
     result = subprocess.check_output(
@@ -50,7 +50,7 @@ path_to_class_txt = st.sidebar.file_uploader(
 if path_to_class_txt is not None:
 
     options = st.sidebar.radio(
-        'Options:', ('Webcam', 'Image', 'Video', 'RTSP'), index=1)
+        'Options:', ('Webcam', 'Image', 'Video', 'RTSP','Screen'), index=1)
 
     gpu_option = st.sidebar.radio(
         'PU Options:', ('CPU', 'GPU'))
@@ -236,7 +236,7 @@ if path_to_class_txt is not None:
     # Web-cam
     if options == 'Webcam':
         cam_options = st.sidebar.selectbox('Webcam Channel',
-                                           ('Select Channel', '0', '1', '2', '3'))
+                                            ('Select Channel', '0', '1', '2', '3'))
         # Model
         if gpu_option == 'CPU':
             model = custom(path_or_model=path_model_file)
@@ -246,89 +246,104 @@ if path_to_class_txt is not None:
         if len(cam_options) != 0:
             if not cam_options == 'Select Channel':
                 cap = cv2.VideoCapture(int(cam_options))
+                
                 stframe1 = st.empty()
                 stframe2 = st.empty()
                 stframe3 = st.empty()
-                while True:
-                    success, img = cap.read()
-                    if not success:
-                        st.error(
-                            f'Webcam channel {cam_options} NOT working\n \
-                            Change channel or Connect webcam properly!!',
-                            icon="🚨"
-                        )
-                        break
-
-                    bbox_list = []
-                    current_no_class = []
-                    results = model(img)
                     
-                    # Bounding Box
-                    box = results.pandas().xyxy[0]
-                    class_list = box['class'].to_list()
 
-                    for i in box.index:
-                        xmin, ymin, xmax, ymax, conf = int(box['xmin'][i]), int(box['ymin'][i]), int(box['xmax'][i]), \
-                            int(box['ymax'][i]), box['confidence'][i]
-                        if conf > confidence:
-                            bbox_list.append([xmin, ymin, xmax, ymax])
-                    if len(bbox_list) != 0:
-                        for bbox, id in zip(bbox_list, class_list):
-                            plot_one_box(bbox, img, label=class_labels[id],
-                                         color=color_pick_list[id], line_thickness=draw_thick)
-                            current_no_class.append([class_labels[id]])
-                    FRAME_WINDOW.image(img, channels='BGR')
+                with mss.mss() as sct:
+                    monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
+                
+                    while True:
+                        screen = sct.grab(monitor)
+                        img = np.array(screen)
+                        
+                        
+                        h, w, channels = img.shape
+                        half2 = h//2
+                        
+                        bottom_whole= img[half2:, :]
+                        img_whole = bottom_whole[230:390, 760:1380]
+                        # success, img = cap.read()
+                        # if not success:
+                        #     st.error(
+                        #         f'Webcam channel {cam_options} NOT working\n \
+                        #         Change channel or Connect webcam properly!!',
+                        #         icon="🚨"
+                        #     )
+                        #     break
 
-                    # FPS
-                    c_time = time.time()
-                    fps = 1 / (c_time - p_time)
-                    p_time = c_time
-                    
-                    # Current number of classes
-                    class_fq = dict(Counter(i for sub in current_no_class for i in set(sub)))
-                    class_fq = json.dumps(class_fq, indent = 4)
-                    class_fq = json.loads(class_fq)
-                    df_fq = pd.DataFrame(class_fq.items(), columns=['Class', 'Number'])
-                    
-                    # Updating Inference results
-                    with stframe1.container():
-                        st.markdown("<h2>Inference Statistics</h2>", unsafe_allow_html=True)
-                        if round(fps, 4)>1:
-                            st.markdown(f"<h4 style='color:green;'>Frame Rate: {round(fps, 4)}</h4>", unsafe_allow_html=True)
-                        else:
-                            st.markdown(f"<h4 style='color:red;'>Frame Rate: {round(fps, 4)}</h4>", unsafe_allow_html=True)
-                    
-                    with stframe2.container():
-                        st.markdown("<h3>Detected objects in curret Frame</h3>", unsafe_allow_html=True)
-                        st.dataframe(df_fq, use_container_width=True)
+                        bbox_list = []
+                        current_no_class = []
+                        results = model(img_whole)
+                        
+                        # Bounding Box
+                        box = results.pandas().xyxy[0]
+                        class_list = box['class'].to_list()
 
-                    with stframe3.container():
-                        st.markdown("<h2>System Statistics</h2>", unsafe_allow_html=True)
-                        js1, js2, js3 = st.columns(3)                       
+                        for i in box.index:
+                            xmin, ymin, xmax, ymax, conf = int(box['xmin'][i]), int(box['ymin'][i]), int(box['xmax'][i]), \
+                                int(box['ymax'][i]), box['confidence'][i]
+                            if conf > confidence:
+                                bbox_list.append([xmin, ymin, xmax, ymax])
+                        if len(bbox_list) != 0:
+                            for bbox, id in zip(bbox_list, class_list):
+                                plot_one_box(bbox, img_whole, label=class_labels[id],
+                                            color=color_pick_list[id], line_thickness=draw_thick)
+                                current_no_class.append([class_labels[id]])
+                        FRAME_WINDOW.image(img_whole, channels='BGR')
 
-                        # Updating System stats
-                        with js1:
-                            st.markdown("<h4>Memory usage</h4>", unsafe_allow_html=True)
-                            mem_use = psutil.virtual_memory()[2]
-                            if mem_use > 50:
-                                js1_text = st.markdown(f"<h5 style='color:red;'>{mem_use}%</h5>", unsafe_allow_html=True)
+                        # FPS
+                        c_time = time.time()
+                        fps = 1 / (c_time - p_time)
+                        p_time = c_time
+                        
+                        # Current number of classes
+                        class_fq = dict(Counter(i for sub in current_no_class for i in set(sub)))
+                        class_fq = json.dumps(class_fq, indent = 4)
+                        class_fq = json.loads(class_fq)
+                        df_fq = pd.DataFrame(class_fq.items(), columns=['Class', 'Number'])
+                        
+                        # Updating Inference results
+                        with stframe1.container():
+                            st.markdown("<h2>Inference Statistics</h2>", unsafe_allow_html=True)
+                            if round(fps, 4)>1:
+                                st.markdown(f"<h4 style='color:green;'>Frame Rate: {round(fps, 4)}</h4>", unsafe_allow_html=True)
                             else:
-                                js1_text = st.markdown(f"<h5 style='color:green;'>{mem_use}%</h5>", unsafe_allow_html=True)
+                                st.markdown(f"<h4 style='color:red;'>Frame Rate: {round(fps, 4)}</h4>", unsafe_allow_html=True)
+                        
+                        with stframe2.container():
+                            st.markdown("<h3>Detected objects in curret Frame</h3>", unsafe_allow_html=True)
+                            st.dataframe(df_fq, use_container_width=True)
 
-                        with js2:
-                            st.markdown("<h4>CPU Usage</h4>", unsafe_allow_html=True)
-                            cpu_use = psutil.cpu_percent()
-                            if mem_use > 50:
-                                js2_text = st.markdown(f"<h5 style='color:red;'>{cpu_use}%</h5>", unsafe_allow_html=True)
-                            else:
-                                js2_text = st.markdown(f"<h5 style='color:green;'>{cpu_use}%</h5>", unsafe_allow_html=True)
+                        with stframe3.container():
+                            st.markdown("<h2>System Statistics</h2>", unsafe_allow_html=True)
+                            js1, js2, js3 = st.columns(3)                       
 
-                        with js3:
-                            st.markdown("<h4>GPU Memory Usage</h4>", unsafe_allow_html=True)  
-                            try:
-                                js3_text = st.markdown(f'<h5>{get_gpu_memory()} MB</h5>', unsafe_allow_html=True)
-                            except:
-                                js3_text = st.markdown('<h5>NA</h5>', unsafe_allow_html=True)
+                            # Updating System stats
+                            with js1:
+                                st.markdown("<h4>Memory usage</h4>", unsafe_allow_html=True)
+                                mem_use = psutil.virtual_memory()[2]
+                                if mem_use > 50:
+                                    js1_text = st.markdown(f"<h5 style='color:red;'>{mem_use}%</h5>", unsafe_allow_html=True)
+                                else:
+                                    js1_text = st.markdown(f"<h5 style='color:green;'>{mem_use}%</h5>", unsafe_allow_html=True)
+
+                            with js2:
+                                st.markdown("<h4>CPU Usage</h4>", unsafe_allow_html=True)
+                                cpu_use = psutil.cpu_percent()
+                                if mem_use > 50:
+                                    js2_text = st.markdown(f"<h5 style='color:red;'>{cpu_use}%</h5>", unsafe_allow_html=True)
+                                else:
+                                    js2_text = st.markdown(f"<h5 style='color:green;'>{cpu_use}%</h5>", unsafe_allow_html=True)
+
+                            with js3:
+                                st.markdown("<h4>GPU Memory Usage</h4>", unsafe_allow_html=True)  
+                                try:
+                                    js3_text = st.markdown(f'<h5>{get_gpu_memory()} MB</h5>', unsafe_allow_html=True)
+                                except:
+                                    js3_text = st.markdown('<h5>NA</h5>', unsafe_allow_html=True)
 
 
     # RTSP
@@ -384,6 +399,113 @@ if path_to_class_txt is not None:
                                      color=color_pick_list[id], line_thickness=draw_thick)
                         current_no_class.append([class_labels[id]])
                 FRAME_WINDOW.image(img, channels='BGR')
+
+                # FPS
+                c_time = time.time()
+                fps = 1 / (c_time - p_time)
+                p_time = c_time
+                
+                # Current number of classes
+                class_fq = dict(Counter(i for sub in current_no_class for i in set(sub)))
+                class_fq = json.dumps(class_fq, indent = 4)
+                class_fq = json.loads(class_fq)
+                df_fq = pd.DataFrame(class_fq.items(), columns=['Class', 'Number'])
+                
+                # Updating Inference results
+                with stframe1.container():
+                    st.markdown("<h2>Inference Statistics</h2>", unsafe_allow_html=True)
+                    if round(fps, 4)>1:
+                        st.markdown(f"<h4 style='color:green;'>Frame Rate: {round(fps, 4)}</h4>", unsafe_allow_html=True)
+                    else:
+                        st.markdown(f"<h4 style='color:red;'>Frame Rate: {round(fps, 4)}</h4>", unsafe_allow_html=True)
+                
+                with stframe2.container():
+                    st.markdown("<h3>Detected objects in curret Frame</h3>", unsafe_allow_html=True)
+                    st.dataframe(df_fq, use_container_width=True)
+
+                with stframe3.container():
+                    st.markdown("<h2>System Statistics</h2>", unsafe_allow_html=True)
+                    js1, js2, js3 = st.columns(3)                       
+
+                    # Updating System stats
+                    with js1:
+                        st.markdown("<h4>Memory usage</h4>", unsafe_allow_html=True)
+                        mem_use = psutil.virtual_memory()[2]
+                        if mem_use > 50:
+                            js1_text = st.markdown(f"<h5 style='color:red;'>{mem_use}%</h5>", unsafe_allow_html=True)
+                        else:
+                            js1_text = st.markdown(f"<h5 style='color:green;'>{mem_use}%</h5>", unsafe_allow_html=True)
+
+                    with js2:
+                        st.markdown("<h4>CPU Usage</h4>", unsafe_allow_html=True)
+                        cpu_use = psutil.cpu_percent()
+                        if mem_use > 50:
+                            js2_text = st.markdown(f"<h5 style='color:red;'>{cpu_use}%</h5>", unsafe_allow_html=True)
+                        else:
+                            js2_text = st.markdown(f"<h5 style='color:green;'>{cpu_use}%</h5>", unsafe_allow_html=True)
+
+                    with js3:
+                        st.markdown("<h4>GPU Memory Usage</h4>", unsafe_allow_html=True)  
+                        try:
+                            js3_text = st.markdown(f'<h5>{get_gpu_memory()} MB</h5>', unsafe_allow_html=True)
+                        except:
+                            js3_text = st.markdown('<h5>NA</h5>', unsafe_allow_html=True)
+
+
+    if options == 'Screen':
+
+        # Model
+        if gpu_option == 'CPU':
+            model = custom(path_or_model=path_model_file)
+        if gpu_option == 'GPU':
+            model = custom(path_or_model=path_model_file, gpu=True)
+
+        stframe1 = st.empty()
+        stframe2 = st.empty()
+        stframe3 = st.empty()
+        
+        scr_height = st.sidebar.number_input('crop bottom',value=1080,step=5)
+        scr_width = st.sidebar.number_input('crop right',value=1920,step=5)
+        
+        move_sideway = st.sidebar.number_input('crop left',step=5)
+        move_vertically = st.sidebar.number_input('crop top',step=5)
+        
+        
+
+        with mss.mss() as sct:
+            monitor = {"top": 0, "left": 0, "width": int(scr_width), "height": int(scr_height)}
+        
+            while True:
+                
+                torch.cuda.empty_cache() #force cude to empty cash so we don't get a memory error
+                
+                screen = sct.grab(monitor)
+                img = np.array(screen)
+                
+                
+                h, w, _ = img.shape
+                
+                img_whole = img[int(move_vertically):h + int(move_vertically), int(move_sideway):w + int(move_sideway)]
+
+                bbox_list = []
+                current_no_class = []
+                results = model(img_whole)
+                
+                # Bounding Box
+                box = results.pandas().xyxy[0]
+                class_list = box['class'].to_list()
+
+                for i in box.index:
+                    xmin, ymin, xmax, ymax, conf = int(box['xmin'][i]), int(box['ymin'][i]), int(box['xmax'][i]), \
+                        int(box['ymax'][i]), box['confidence'][i]
+                    if conf > confidence:
+                        bbox_list.append([xmin, ymin, xmax, ymax])
+                if len(bbox_list) != 0:
+                    for bbox, id in zip(bbox_list, class_list):
+                        plot_one_box(bbox, img_whole, label=class_labels[id],
+                                    color=color_pick_list[id], line_thickness=draw_thick)
+                        current_no_class.append([class_labels[id]])
+                FRAME_WINDOW.image(img_whole, channels='BGR')
 
                 # FPS
                 c_time = time.time()
